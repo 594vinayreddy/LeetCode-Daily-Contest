9200c2b6a63a3a980dbeb5c0df7ddf204fcd355e
Vinayreddy R Hosagowdar <2712vinayreddy30@gmail.com>
2026-08-11 11:12:10 +0530

    Time: 0 ms (100%), Space: 12.4 MB (38.64%) - LeetHub

--- 0877-stone-game/0877-stone-game.py
+++ 0877-stone-game/0877-stone-game.py
@@ -4,12 +4,4 @@ class Solution(object):
         :type piles: List[int]
         :rtype: bool
         """
-        n = len(piles)
-        dp = piles[:]
-        
-        for length in range(2, n + 1):
-            for i in range(n - length + 1):
-                j = i + length - 1
-                dp[i] = max(piles[i] - dp[i + 1], piles[j] - dp[i])
-        
-        return dp[0] >= 0
+        return True
